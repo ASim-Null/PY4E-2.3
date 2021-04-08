23414abfaac6e60978ecc7d0623b75a153c48959
ASim-Null <51908131+ASim-Null@users.noreply.github.com>
2021-04-08 14:46:24 +0100

    Create iterationVariables.py
    Examples of iteration variables #1

---
+++ iterationVariables.py
@@ -0,0 +1,10 @@
+# the iteration variable, otherwise known as 'i' moves through all the values in a sequence. 
+for i in [5,4,3,2,1]:
+  print(i)
+#this is useful for when you want to have a look at each bit of code individually. 
+#can also be used as a different term. in a list for example. 
+
+fruit= [apple, banana, orange, strawberry, pear, grape]
+for fruits in fruit:
+  print(fruits)
+  
